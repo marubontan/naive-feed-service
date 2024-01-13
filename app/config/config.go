31c3ec24e4665ecf02e81f69ee48eb3061166ac2
Shuhei Kishi <marubontan@gmail.com>
2024-01-13 10:58:38 -0300

    add basic components (#4)

--- app/config/config.go
+++ app/config/config.go
@@ -6,11 +6,19 @@ import (
 
 type Config struct {
 	Server Server
+	Db     Db
 }
 
 type Server struct {
 	Address string `envconfig:"ADDRESS" default:"0.0.0.0"`
 	Port    string `envconfig:"PORT" default:"8080"`
+}
+
+type Db struct {
+	Host     string `envconfig:"DB_HOST" default:"localhost"`
+	Port     string `envconfig:"DB_PORT" default:"5432"`
+	User     string `envconfig:"DB_USER" default:"postgres"`
+	Password string `envconfig:"DB_PASSWORD" default:"postgres"`
 }
 
 var config Config
