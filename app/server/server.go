c5c105f80f6f97630325e238fd422a49799bf596
Shuhei Kishi <marubontan@gmail.com>
2024-01-14 09:35:59 -0300

    add simple swagger integration (#6)

--- app/server/server.go
+++ app/server/server.go
@@ -2,7 +2,12 @@ package server
 
 import (
 	"fmt"
+	_ "naive-feed-service/app/cmd/docs"
 	"naive-feed-service/app/config"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
 	infrastructure "naive-feed-service/app/infrastructure/repository"
 	"naive-feed-service/app/usecase"
 	"naive-feed-service/app/util"
@@ -18,64 +23,102 @@ type SaveFeedRequest struct {
 	ItemId string `json:"item_id"`
 }
 
-func Run() {
-	util.Logger.Println("Starting server...")
+type Server struct {
+	config *config.Config
+	engine *gin.Engine
+	db     *gorm.DB
+}
+
+func NewServer() *Server {
 	config, err := config.Load()
 	if err != nil {
 		util.Logger.Fatal(err)
 	}
 	db, err := gorm.Open(postgres.Open(fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%s sslmode=disable", config.Db.Host, config.Db.User, config.Db.Password, config.Db.Port)), &gorm.Config{})
-	db.AutoMigrate(&infrastructure.FeedItem{})
 	if err != nil {
 		util.Logger.Fatal(err)
 	}
-	engine := gin.Default()
-	engine.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
+
+	return &Server{
+		config: config,
+		engine: gin.Default(),
+		db:     db,
+	}
+}
+
+// @Summary Check health
+// @Description Check health
+// @Success 200
+// @Router /health [get]
+func (s *Server) healthHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "healthy",
 	})
+}
 
-	engine.POST("/feed", func(c *gin.Context) {
-		var requestData SaveFeedRequest
+// @Summary Update feed
+// @Description Update feed
+// @Success 200
+// @Router /feed [put]
+func (s *Server) updateFeedHandler(c *gin.Context) {
+	feedRepository := infrastructure.NewFeedRepository(s.db)
+	useCase := usecase.NewUpdateFeedUsecase(feedRepository)
+	err := useCase.Run()
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	responseData := gin.H{"message": "Received PUT request"}
+	c.JSON(200, responseData)
 
-		if err := c.ShouldBindJSON(&requestData); err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
-		feedRepository := infrastructure.NewFeedRepository(db)
-		useCase := usecase.NewSaveFeedItemUsecase(feedRepository)
-		id, err := useCase.Run(&usecase.SaveFeedItemInputDTO{
-			ItemId:    requestData.ItemId,
-			CreatedAt: time.Now(),
-		})
-		if err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
-		responseData := gin.H{"message": "Received POST request", "id": id}
-		c.JSON(200, responseData)
+}
 
+// @Summary Post feed item
+// @Description Update feed
+// @Param data body SaveFeedRequest true "Item to add to feed"
+// @Success 200
+// @Router /feed [post]
+func (s *Server) postFeedItemHandler(c *gin.Context) {
+	var requestData SaveFeedRequest
+
+	if err := c.ShouldBindJSON(&requestData); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	feedRepository := infrastructure.NewFeedRepository(s.db)
+	useCase := usecase.NewSaveFeedItemUsecase(feedRepository)
+	id, err := useCase.Run(&usecase.SaveFeedItemInputDTO{
+		ItemId:    requestData.ItemId,
+		CreatedAt: time.Now(),
 	})
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	responseData := gin.H{"message": "Received POST request", "id": id}
+	c.JSON(200, responseData)
+}
 
-	engine.GET("/feed", func(c *gin.Context) {
-		feedRepository := infrastructure.NewFeedRepository(db)
-		useCase := usecase.NewGetFeedUsecase(feedRepository)
-		feedItems := useCase.Run()
-		c.JSON(200, feedItems)
-	})
+// @Summary Get feed
+// @Description get feed
+// @Success 200
+// @Router /feed [get]
+func (s *Server) getFeedHandler(c *gin.Context) {
+	feedRepository := infrastructure.NewFeedRepository(s.db)
+	useCase := usecase.NewGetFeedUsecase(feedRepository)
+	feedItems := useCase.Run()
+	c.JSON(200, feedItems)
+}
 
-	engine.PUT("/feed", func(c *gin.Context) {
-		feedRepository := infrastructure.NewFeedRepository(db)
-		useCase := usecase.NewUpdateFeedUsecase(feedRepository)
-		err := useCase.Run()
-		if err != nil {
-			c.JSON(400, gin.H{"error": err.Error()})
-			return
-		}
-		responseData := gin.H{"message": "Received PUT request"}
-		c.JSON(200, responseData)
-	})
-	engine.Run(fmt.Sprint(config.Server.Address, ":", config.Server.Port))
+// @title Naive Feed Service
+func (s *Server) Run() {
+	util.Logger.Println("Starting server...")
+	s.db.AutoMigrate(&infrastructure.FeedItem{})
+	s.engine.GET("/health", s.healthHandler)
+	s.engine.POST("/feed", s.postFeedItemHandler)
+	s.engine.GET("/feed", s.getFeedHandler)
+	s.engine.PUT("/feed", s.updateFeedHandler)
+	s.engine.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	s.engine.Run(fmt.Sprint(s.config.Server.Address, ":", s.config.Server.Port))
 
 }
