bb42f163b9a6ce865b5416a178f2143f2aace3e8
Shuhei Kishi <marubontan@gmail.com>
2024-02-25 11:42:51 -0300

    Feat/switch to slog (#11)
    * switch to slog
    
    * add slog

--- app/server/server.go
+++ app/server/server.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"fmt"
+	"log/slog"
 	_ "naive-feed-service/app/cmd/docs"
 	"naive-feed-service/app/config"
+	"os"
 
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -32,12 +34,17 @@ type Server struct {
 func NewServer() *Server {
 	config, err := config.Load()
 	if err != nil {
-		util.Logger.Fatal(err)
+		util.Logger.Error("Failed to load config", slog.Any("err", err))
+		os.Exit(1)
 	}
+	util.Logger.Info("Loaded config")
+
 	db, err := gorm.Open(postgres.Open(fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%s sslmode=disable", config.Db.Host, config.Db.User, config.Db.Password, config.Db.Port)), &gorm.Config{})
 	if err != nil {
-		util.Logger.Fatal(err)
+		util.Logger.Error("Failed to connect to database", slog.Any("err", err))
+		os.Exit(1)
 	}
+	util.Logger.Info("Connected to database")
 
 	return &Server{
 		config: config,
@@ -51,6 +58,7 @@ func NewServer() *Server {
 // @Success 200
 // @Router /health [get]
 func (s *Server) healthHandler(c *gin.Context) {
+	util.Logger.Info("Health check called")
 	c.JSON(http.StatusOK, gin.H{
 		"message": "healthy",
 	})
@@ -61,15 +69,18 @@ func (s *Server) healthHandler(c *gin.Context) {
 // @Success 200
 // @Router /feed [put]
 func (s *Server) updateFeedHandler(c *gin.Context) {
+	util.Logger.Info("Update feed called")
 	feedRepository := infrastructure.NewFeedRepository(s.db)
 	useCase := usecase.NewUpdateFeedUsecase(feedRepository)
 	err := useCase.Run()
 	if err != nil {
+		util.Logger.Info("Failed to update feed", slog.Any("err", err))
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 	responseData := gin.H{"message": "Received PUT request"}
 	c.JSON(200, responseData)
+	util.Logger.Info("Updated feed")
 
 }
 
@@ -79,9 +90,11 @@ func (s *Server) updateFeedHandler(c *gin.Context) {
 // @Success 200
 // @Router /feed [post]
 func (s *Server) postFeedItemHandler(c *gin.Context) {
+	util.Logger.Info("Post feed item called")
 	var requestData SaveFeedRequest
 
 	if err := c.ShouldBindJSON(&requestData); err != nil {
+		util.Logger.Error("Failed to bind JSON", slog.Any("err", err))
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
@@ -97,6 +110,7 @@ func (s *Server) postFeedItemHandler(c *gin.Context) {
 	}
 	responseData := gin.H{"message": "Received POST request", "id": id}
 	c.JSON(200, responseData)
+	util.Logger.Info("Posted feed item")
 }
 
 // @Summary Get feed
@@ -104,15 +118,17 @@ func (s *Server) postFeedItemHandler(c *gin.Context) {
 // @Success 200
 // @Router /feed [get]
 func (s *Server) getFeedHandler(c *gin.Context) {
+	util.Logger.Info("Get feed called")
 	feedRepository := infrastructure.NewFeedRepository(s.db)
 	useCase := usecase.NewGetFeedUsecase(feedRepository)
 	feedItems := useCase.Run()
 	c.JSON(200, feedItems)
+	util.Logger.Info("Got feed")
 }
 
 // @title Naive Feed Service
 func (s *Server) Run() {
-	util.Logger.Println("Starting server...")
+	util.Logger.Info("Starting server...")
 	s.db.AutoMigrate(&infrastructure.FeedItem{})
 	s.engine.GET("/health", s.healthHandler)
 	s.engine.POST("/feed", s.postFeedItemHandler)
